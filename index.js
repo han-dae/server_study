d6d3b3aa671d615067064151738e11ffad7a2ca9
dae <h33223012@naver.com>
2021-11-02 06:44:03 +0900

    second

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const mongoose = require("mongoose");
+const { User } = require("./models/index");
+const bodyParser = require("body-parser");
+const config = require("./config/key");
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 mongoose
   .connect(
     "mongodb+srv://h33223012:iuhdcsc2359@cluster0.zds3d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
@@ -15,7 +20,17 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!~안녕하세요");
+  res.send("Hello World!~");
+});
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
